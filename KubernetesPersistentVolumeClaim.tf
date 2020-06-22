f793255d1345e9b41151306309a723fc1716f463
Hemant Seth <Hemant.Seth@cpa.texas.gov>
2020-06-21 22:46:03 -0400

    Fixed the syntax error

--- KubernetesPersistentVolumeClaim.tf
+++ KubernetesPersistentVolumeClaim.tf
@@ -1,7 +1,6 @@
 resource "kubernetes_persistent_volume_claim" "mysql-persistent-volume-claim" {
   metadata {
     name = "mysql-persistent-volume-claim"
-	namespace = kubernetes_namespace.n.metadata[0].name
   }
   spec {
     access_modes = ["ReadWriteOnce"]
